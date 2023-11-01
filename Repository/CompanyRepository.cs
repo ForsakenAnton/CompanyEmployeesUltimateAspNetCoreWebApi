d1578426c3a72f44915511f524ee88c736106585
Anton <forsakenanton@gmail.com>
2023-11-01 13:19:35 +0200

    Getting All Companies From the Database

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -9,4 +9,9 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     : base(repositoryContext)
     {
     }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
 }
