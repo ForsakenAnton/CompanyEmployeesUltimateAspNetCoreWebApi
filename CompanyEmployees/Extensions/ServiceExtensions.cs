c78965dfa4a20d0a7ad443555b54a992e8b588c7
Anton <forsakenanton@gmail.com>
2023-11-01 11:37:46 +0200

    Registering RepositoryContext at a Runtime

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -3,6 +3,7 @@ using LoggerService;
 using Repository;
 using Service.Contracts;
 using Service;
+using Microsoft.EntityFrameworkCore;
 
 namespace CompanyEmployees.Extensions;
 
@@ -31,4 +32,16 @@ public static class ServiceExtensions
 
     public static void ConfigureServiceManager(this IServiceCollection services) =>
         services.AddScoped<IServiceManager, ServiceManager>();
+
+    public static void ConfigureSqlContext(
+        this IServiceCollection services,
+        IConfiguration configuration) 
+    {
+        //services.AddSqlServer<RepositoryContext>(
+        //    configuration.GetConnectionString("sqlConnection"));
+
+        services
+            .AddDbContext<RepositoryContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+    }
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -15,6 +15,7 @@ builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 builder.Services.AddControllers();
 
