3a832002b35618ae887b759c8e47cec0002b1b28
Anton <forsakenanton@gmail.com>
2023-11-03 12:29:08 +0200

    Restricting Media Types

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -24,6 +24,7 @@ builder.Services
     .AddControllers(config =>
     {
         config.RespectBrowserAcceptHeader = true;
+        config.ReturnHttpNotAcceptable = true;
     })
     .AddXmlDataContractSerializerFormatters()
     .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
