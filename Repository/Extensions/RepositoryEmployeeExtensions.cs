61526b39112eda6b18833346cbe2db3aaf443377
Anton <forsakenanton@gmail.com>
2023-11-10 10:51:44 +0200

    How to Implement Sorting in ASP.NET Core Web API

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,4 +1,9 @@
-﻿using Entities.Models;
+﻿
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Text;
+using Entities.Models;
+
 
 namespace Repository.Extensions;
 
@@ -12,6 +17,7 @@ public static class RepositoryEmployeeExtensions
         return employees.Where(e =>
             e.Age >= minAge && e.Age <= maxAge);
     }
+
 
     public static IQueryable<Employee> Search(
         this IQueryable<Employee> employees,
@@ -28,5 +34,51 @@ public static class RepositoryEmployeeExtensions
 
         return employees.Where(e =>
             e.Name!.ToLower().Contains(lowerCaseTerm));
+    }
+
+
+    public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string? orderByQueryString)
+    {
+        if (string.IsNullOrWhiteSpace(orderByQueryString))
+        {
+            return employees.OrderBy(e => e.Name);
+        }
+
+        string[] orderParams = orderByQueryString
+            .Trim()
+            .Split(',');
+
+        PropertyInfo[] propertyInfos = typeof(Employee)
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        var orderQueryBuilder = new StringBuilder();
+
+        foreach (string? param in orderParams)
+        {
+            if (string.IsNullOrWhiteSpace(param))
+            {
+                continue;
+            }
+
+            var propertyFromQueryName = param.Split(" ")[0];
+
+            PropertyInfo? objectProperty = propertyInfos.FirstOrDefault(pi =>
+                pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+            if (objectProperty == null)
+            {
+                continue;
+            }
+
+            string direction = param.EndsWith(" desc") ? "descending" : "ascending";
+            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
+        }
+
+        string orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+        if (string.IsNullOrWhiteSpace(orderQuery))
+        {
+            return employees.OrderBy(e => e.Name);
+        }
+
+        return employees.OrderBy(orderQuery);
     }
 }
