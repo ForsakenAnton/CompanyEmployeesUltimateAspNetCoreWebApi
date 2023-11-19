f0e66d3cbec27c0b7f41a2cc6668757f71bc9456
Anton <forsakenanton@gmail.com>
2023-11-19 14:10:46 +0200

    Required Package Installation and Configuration

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -83,4 +83,17 @@ public static class ServiceExtensions
             }
         });
     }
+
+
+    // use Asp.Versioning.Mvc
+    // Microsoft.AspNetCore.Mvc.Versioning - Deprecated
+    public static void ConfigureVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(opt =>
+        {
+            opt.ReportApiVersions = true;
+            opt.AssumeDefaultVersionWhenUnspecified = true;
+            opt.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
+        });
+    }
 }
